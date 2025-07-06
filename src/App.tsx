9d66e525c638104d3f0027134a27d9833851a8a3
Avinash Shrivastav <avinash.shrivastav@revv.co.in>
2025-07-06 15:26:45 +0530

    Your commit message

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import "./App.css";
 export default function App() {
   return (
     <h1>
-      
+      <span className="text-3xl font-bold underline">
+        Hello, Vite + React + TypeScript + Tailwind CSS!
+      </span>
     </h1>
   );
 }
